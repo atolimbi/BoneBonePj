e450c9657bcfd10ba026751f57ca278beadb8717
Atolimbi <leonato19@gmail.com>
2017-10-31 23:48:14 -0400

    Prueba con Branch BoneBoneTest

---
+++ BoneBonePj/Assets/Scripts/TestGit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Debug.Log ("testGit");
+	}
+}

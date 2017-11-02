8566b2f48179e3d212871ed456db9342a2211947
Atolimbi <leonato19@gmail.com>
2017-11-01 23:08:33 -0400

    Adicion de balas en pantalla y municion del cañon. Adicion de nuevos sprites de bombas

--- BoneBonePj/Assets/Scripts/Cannon/CanonScript.cs
+++ BoneBonePj/Assets/Scripts/Cannon/CanonScript.cs
@@ -7,16 +7,13 @@ public class CanonScript : MonoBehaviour {
 
 	public int forceBullet;
 
-	private Transform gunner;
 	private BulletManager bulletManager;
-
-
+	private Transform gunner;
 	// Use this for initialization
 	void Start () {
 
 		gunner = this.gameObject.transform.GetChild (0); //transform of the rotation part of the cannon
 		bulletManager = GameObject.FindObjectOfType<BulletManager> (); // Get the Bullet Manager Gameobject
-	
 	}
 	
 	// Update is called once per frame
@@ -25,7 +22,9 @@ public class CanonScript : MonoBehaviour {
 		RotateCannon ();
 
 		if (Input.GetMouseButtonDown (0)) {
-			ShootBullet ();
+			if (bulletManager.ammoCannon >= 1) {
+				ShootBullet ();
+			}
 		}
 	
 	}
@@ -36,20 +35,15 @@ public class CanonScript : MonoBehaviour {
 		float angle = Mathf.Atan2 (direction.y,direction.x) * Mathf.Rad2Deg;
 		Quaternion rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
 		gunner.transform.rotation = Quaternion.Slerp(gunner.transform.rotation, rotation, 0.05f );
-		//gunner.transform.rotation = Quaternion.Euler(0,0,angle-90);
-		//gunner.transform.rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
-
-
-
-		//gunner.transform.LookAt (direction, Vector3.forward);
 
 	}
 
 	void ShootBullet(){
-
+		
+		bulletManager.ammoCannon--;
+		bulletManager.bulletsOnScreen++;
 		GameObject bullet = Instantiate (bulletManager.bulletOnCannon,gunner.position,gunner.rotation) as GameObject;
 		Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D> ();
 		rbBullet.AddForce (gunner.up*forceBullet,ForceMode2D.Force);
-		//Debug.Log ("Shoooooot!!!!");
 	}
 }
